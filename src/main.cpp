a18414f4186a79a55a4b7709cbc3fb365643e629
Quentin Barbarat <quentin.barbarat@epita.fr>
2021-05-10 21:42:08 +0200

    add pseudo timer

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include "bn_bg_palettes.h"
 #include "bn_sprite_ptr.h"
 #include "bn_sprite_text_generator.h"
+#include "bn_timer.h"
+#include "bn_log.h"
 #include "bn_vector.h"
 
 #include "gj_big_sprite_font.h"
+
+#include "bn_music_items.h"
 
 int main()
 {
@@ -19,8 +23,26 @@ int main()
 
     text_generator.generate(0, 0, "Hello, World!", text_sprites);
 
+    bn::music_items::midiboi.play(0.0);
+
+    bn::timer timer;
+
+    timer.restart();
+
+    BN_LOG("Test", "3");
+
+    const int TICKS_PER_SECONDS = 262123;
+
+    int seconds = 0;
+
     while(true)
     {
         bn::core::update();
+        if (timer.elapsed_ticks() >= TICKS_PER_SECONDS)
+        {
+            seconds += 1;
+            timer.restart();
+        }
+        BN_LOG("Seconds: ", seconds);
     }
 }
